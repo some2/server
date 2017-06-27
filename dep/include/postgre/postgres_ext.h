97d38703e30c193a66555c443e2cd87707688be1
some2 <a1816530@mvrht.net>
2017-06-27 10:32:15 +0200

    Revert "Update to libpg-9.6.2"

--- dep/include/postgre/postgres_ext.h
+++ dep/include/postgre/postgres_ext.h
@@ -7,7 +7,7 @@
  *	For example, the Oid type is part of the API of libpq and other libraries.
  *
  *	   Declarations which are specific to a particular interface should
- *	go in the header file for that interface (such as libpq-fe.h).  This
+ *	go in the header file for that interface (such as libpq-fe.h).	This
  *	file is only for fundamental Postgres declarations.
  *
  *	   User-written C functions don't count as "external to Postgres."
@@ -15,7 +15,7 @@
  *	use header files that are otherwise internal to Postgres to interface
  *	with the backend.
  *
- * src/include/postgres_ext.h
+ * $PostgreSQL: pgsql/src/include/postgres_ext.h,v 1.17 2007/02/06 09:16:08 petere Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -37,9 +37,6 @@ typedef unsigned int Oid;
 #define OID_MAX  UINT_MAX
 /* you will need to include <limits.h> to use the above #define */
 
-/* Define a signed 64-bit integer type for use in client API declarations. */
-typedef PG_INT64_TYPE pg_int64;
-
 
 /*
  * Identifiers of error message fields.  Kept here to keep common
@@ -47,7 +44,6 @@ typedef PG_INT64_TYPE pg_int64;
  * applications.
  */
 #define PG_DIAG_SEVERITY		'S'
-#define PG_DIAG_SEVERITY_NONLOCALIZED 'V'
 #define PG_DIAG_SQLSTATE		'C'
 #define PG_DIAG_MESSAGE_PRIMARY 'M'
 #define PG_DIAG_MESSAGE_DETAIL	'D'
@@ -56,13 +52,8 @@ typedef PG_INT64_TYPE pg_int64;
 #define PG_DIAG_INTERNAL_POSITION 'p'
 #define PG_DIAG_INTERNAL_QUERY	'q'
 #define PG_DIAG_CONTEXT			'W'
-#define PG_DIAG_SCHEMA_NAME		's'
-#define PG_DIAG_TABLE_NAME		't'
-#define PG_DIAG_COLUMN_NAME		'c'
-#define PG_DIAG_DATATYPE_NAME	'd'
-#define PG_DIAG_CONSTRAINT_NAME 'n'
 #define PG_DIAG_SOURCE_FILE		'F'
 #define PG_DIAG_SOURCE_LINE		'L'
 #define PG_DIAG_SOURCE_FUNCTION 'R'
 
-#endif   /* POSTGRES_EXT_H */
+#endif
