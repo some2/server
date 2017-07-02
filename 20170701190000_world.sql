20d32d73133a8582444346cf621647300f9c0401
some2 <a1816530@mvrht.net>
2017-07-02 21:26:18 +0200

    Fix typo

--- 20170701190000_world.sql
+++ 20170701190000_world.sql
@@ -1,6 +1,6 @@
 INSERT INTO `migrations` VALUES ('20170701190000'); 
 
--- Set ItemeLevel for Reins of the Winterspring Frostsaber (ID: 13086) to 1000g:
+-- Set BuyPrice for Reins of the Winterspring Frostsaber (ID: 13086) to 1000g:
 UPDATE item_template SET BuyPrice = 10000000 WHERE entry = 13086;
 
 -- Set RequiredLevel for Reins of the Winterspring Frostsaber (ID: 13086) to 60:
