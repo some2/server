f0979a7d7627dee5f91ec6fdd731e88f5fabf24d
some2 <a1816530@mvrht.net>
2017-08-18 11:38:57 +0200

    Add files via upload

---
+++ 20170818093600_world.sql
@@ -0,0 +1,4 @@
+INSERT INTO `migrations` VALUES ('20170818093600'); 
+
+-- Add Fel Infused Leggings (ID: 19133) to Lord Kazzak (ID: 12397) loot table:
+INSERT INTO creature_loot_template VALUES (12397, 19133, 9.04, 0, 1, 1, 0);
